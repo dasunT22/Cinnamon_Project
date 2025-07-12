70a755b9f95099323ef104671a97c99f95e22bae
dasunT22 <dasuntharinda2535@gmail.com>
2025-07-12 11:18:48 +0530

    finalize customer ui's

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
-
-
 import { useNavigate } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import AboutUs from "../AboutUs/AboutUs";
+import ContactUs from "../ContactUs/ContactUs";
+import FactoryTour from "../FactoryTour/FactoryTour";
 import Shop from "../Shop/Shop";
 import "./Home.css";
-import FactoryTour from "../FactoryTour/FactoryTour";
-import ContactUs from "../ContactUs/ContactUs";
-import Footer from "../../components/Footer/Footer";
 
 
 const Home = () => {
@@ -42,20 +40,21 @@ const Home = () => {
         <div style={{ marginTop: 48, marginBottom: 0, zIndex: 1, position: "relative" }}>
         <FactoryTour />
       </div> */}
-      <div>
+
+      <div style={{ marginTop: -75, marginBottom: 0, zIndex: 1, position: "relative", background: '#fff' }}>
         <Shop />
       </div>
-      <div>
+      <div style={{ marginTop: 0, marginBottom: 0, zIndex: 1, position: "relative", background: '#fff' }}>
         <AboutUs />
       </div>
-      <div>
+      <div style={{ marginTop: 0, marginBottom: 0, zIndex: 1, position: "relative", background: '#fff' }}>
         <FactoryTour />
       </div>
-      <div>
+      <div style={{ marginTop: 0, marginBottom: 0, zIndex: 1, position: "relative", background: '#fff' }}>
         <ContactUs />
       </div>
-      <div>
-      <Footer />
+      <div style={{ marginTop: -20, marginBottom: 0, zIndex: 1, position: "relative", background: '#fff' }}>
+        <Footer />
       </div>
     </>
   );
